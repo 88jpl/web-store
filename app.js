0af5b2bffeff0714889c0c70a2e30af8732e7b25
Justin-mac <ljustin515@gmail.com>
2022-06-27 11:46:51 -0600

    functioning welcome store and checkout pages

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-Vue.component("component",
-    {
-        template: ``
+const API_URL = "https://fakestoreapi.com";
+
+Vue.component("product", {
+    template: `
+    <div>
+        <div>
+            <img v-bind:src="item.image">
+            {{ item.price }}
+        </div>
+    </div>
+    `,
+    props: [
+        "item"
+    ]
     });
 
 var app = new Vue({
     el: "#app",
     data: {
-        
+        page: "welcome",
+        products: []
+    },
+    methods: {
+        getProducts: async function () {
+            let response = await fetch(`${API_URL}/products`);
+            let data = await response.json();
+
+            this.products = data;
+        },
+        setPage: function (page) {
+            this.page = page;
+        }
+    },
+    created: function () {
+        this.getProducts();
     }
 });
