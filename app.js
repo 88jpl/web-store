621f980bd61402c589ca93736c9402d002f31925
Justin-mac <ljustin515@gmail.com>
2022-06-28 12:44:23 -0600

    new component for cart page

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ Vue.component("product", {
         }
     }
 });
+Vue.component("cart-page", {
+    template: `
+    <div class="products">
+        <div>Total {{carttotal}}</div>
+        <div>
+            <img v-bind:src="item.image">
+            {{ item.price }}
+            <button v-on:click="addToShoppingCart(item)" >Add to cart</button>
+        </div>
+    </div>
+    `,
+    props: [
+        "item",
+        "cart",
+        "carttotal"
+    ],
+    methods: {
+        addToShoppingCart: function() {
+            this.cart.push(this.item);
+        }
+    }
+});
 
 var app = new Vue({
     el: "#app",
@@ -47,5 +69,17 @@ var app = new Vue({
     },
     created: function () {
         this.getProducts();
+    },
+    computed: {
+        cartTotal: function() {
+            let total = 0;
+            this.Vuecart.forEach(product =>{
+                total += product.price;
+            });
+            return total;
+        },
+        cartTax: function() {
+            return this.cartTotal * .08;
+        }
     }
 });
